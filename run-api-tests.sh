7704bdf356f4960a3e5a6805c2b7775868a9d9d2
Ash <ashkumark@gmail.com>
2023-11-09 21:50:15 +0000

    Tracing

--- run-api-tests.sh
+++ run-api-tests.sh
@@ -35,6 +35,7 @@ docker-compose -f docker-compose-api.yaml rm -f
 
 # Starting new stack environment
 docker-compose -f docker-compose-api.yaml up -d --no-color --build
+docker-compose -f docker-compose-api.yaml ps
 echo "* UP - sleeping for 180s.. check container"
 sleep 180s
 
@@ -43,6 +44,7 @@ sleep 180s
 #chmod -R ug+rwx /home/jenkins/target
 
 docker-compose -f docker-compose-api.yaml run --rm -e TYPE="@API" -u ${HOST_UID_GID} --entrypoint="./runner-api.sh" api-test-service
+docker-compose -f docker-compose-api.yaml ps
 #docker-compose -f docker-compose-api.yaml run --rm -e TYPE="@API" -u ${HOST_UID_GID} --entrypoint="./runner-api.sh" -v "$PWD:/home/jenkins" -v "$HOME/.m2:/root/.m2" -v "$PWD/target:/home/jenkins/target" api-test-service
 
 #docker-compose -p $COMPOSE_ID -f docker-compose-api.yaml run -e TYPE="@API" -u ${HOST_UID_GID} api-test-service --name "api"
