35db6ac5e83a2087f3ab9f9fd10bd81fed9ba751
Ash <ashkumark@gmail.com>
2023-11-11 15:18:00 +0000

    new Jenkinsfile

--- run-api-tests.sh
+++ run-api-tests.sh
@@ -46,7 +46,7 @@ docker-compose -f docker-compose-api.yaml rm -f
 #chmod -R ug+rwx /home/jenkins/target
 
 echo "* RUN "
-docker-compose -f docker-compose-api.yaml run --rm -e TYPE="@API" -u ${HOST_UID_GID} --entrypoint="./runner-api.sh" api-test-service
+#docker-compose -f docker-compose-api.yaml run --rm -e TYPE="@API" -u ${HOST_UID_GID} --entrypoint="./runner-api.sh" api-test-service
 #docker-compose -f docker-compose-api.yaml run --rm -e TYPE="@API" -u ${HOST_UID_GID} --entrypoint="./runner-api.sh" -v "$PWD/target:/home/jenkins/target" api-test-service
 #docker-compose -f docker-compose-api.yaml ps
 
