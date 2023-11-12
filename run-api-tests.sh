bee90b212d18b8e4e01666996c7108f8e192a54a
Ash <ashkumark@gmail.com>
2023-11-12 10:19:17 +0000

    Updated Jenkinsfile

--- run-api-tests.sh
+++ run-api-tests.sh
@@ -46,11 +46,11 @@ docker-compose -f docker-compose-api.yaml rm -f
 #chmod -R ug+rwx /home/jenkins/target
 
 echo "* RUN "
-#docker-compose -f docker-compose-api.yaml run --rm -e TYPE="@API" -u ${HOST_UID_GID} --entrypoint="./runner-api.sh" api-test-service
+docker-compose -f docker-compose-api.yaml run --rm -e TYPE="@API" -u ${HOST_UID_GID} --entrypoint="./runner-api.sh" api-test-service
 #docker-compose -f docker-compose-api.yaml run --rm -e TYPE="@API" -u ${HOST_UID_GID} --entrypoint="./runner-api.sh" -v "$PWD/target:/home/jenkins/target" api-test-service
 #docker-compose -f docker-compose-api.yaml ps
 
-docker-compose -f /var/jenkins_home/workspace/maven-9/docker-compose-api.yaml run --rm -e TYPE="@API" -u ${HOST_UID_GID} --entrypoint="./runner-api.sh" api-test-service
+#docker-compose -f /var/jenkins_home/workspace/maven-9/docker-compose-api.yaml run --rm -e TYPE="@API" -u ${HOST_UID_GID} --entrypoint="./runner-api.sh" api-test-service
 
 #docker-compose -f docker-compose-api.yaml run --rm -e TYPE="@API" -u ${HOST_UID_GID} --entrypoint="./runner-api.sh" -v "$PWD:/home/jenkins" -v "$HOME/.m2:/root/.m2" -v "$PWD/target:/home/jenkins/target" api-test-service
 
