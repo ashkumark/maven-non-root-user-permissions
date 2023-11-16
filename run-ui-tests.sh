b4304ea71654d56e2b16df6427a4fa3578c2601b
Ash <ashkumark@gmail.com>
2023-11-16 18:02:01 +0000

    Browser params

--- run-ui-tests.sh
+++ run-ui-tests.sh
@@ -6,7 +6,7 @@ export HOST_UID_GID=$JENKINS_USER_ID:$JENKINS_GROUP_ID
 docker-compose -f docker-compose-api.yaml rm -f
 
 # Starting new stack environment
-echo "* RUN "
+echo "* RUN UI tests using ${BROWSER}"
 #docker-compose -f docker-compose-ui.yaml run -e TYPE="@UI" -e BROWSER="chrome" -u ${HOST_UID_GID} ui-test-service
 #docker-compose -f docker-compose-ui.yaml run -e TYPE="@UI" -e BROWSER="firefox" -u ${HOST_UID_GID} ui-test-service
 
