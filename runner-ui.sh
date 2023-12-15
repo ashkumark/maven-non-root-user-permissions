c075c73e3d54e34ca6cd7f925ac183f6e5f464d9
Ash <ashkumark@gmail.com>
2023-12-15 11:47:53 +0000

    sh

--- runner-ui.sh
+++ runner-ui.sh
@@ -16,22 +16,22 @@ echo "Browser - $BROWSER"
 echo "Validate hub status.."
 
 STATUS=false
-selenium_hub=192.168.60.2
+selenium_hub_ip=192.168.60.2
 port=32001
-hub_host_url=http://$selenium_hub:$port/wd/hub
+hub_host_url=http://${selenium_hub_ip}:${port}/wd/hub
 
 echo "Loop until the hub status is true.."
 while [ $STATUS != true ]
 do
  sleep 1
  #STATUS=$(curl -s http://selenium-hub:4444/wd/hub/status | jq -r .value.ready)
- STATUS=$(curl -s http://$selenium-hub:$port/wd/hub/status | jq -r .value.ready)
+ STATUS=$(curl -s ${hub_host_url}/status | jq -r .value.ready)
  echo "Status of Selenium Hub is - $STATUS"
 done
 
 # Run tests
 #mvn test -Dtest=TestRunnerUI -Dcucumber.filter.tags=$TYPE -DHUB_HOST=$HUB_HOST -DBROWSER=$BROWSER
-mvn test -Dtest=TestRunnerUI -Dcucumber.filter.tags=@UI -DHUB_HOST=$hub_host_url -DBROWSER=$BROWSER
+mvn test -Dtest=TestRunnerUI -Dcucumber.filter.tags=@UI -DHUB_HOST=${hub_host_url} -DBROWSER=$BROWSER
 
 
 #docker ps
